a54840ff0d0b5b1b326e0c8588f11b98d29ebc3c
Leandro Klyte <klyte45@hotmail.com>
2022-09-16 10:11:03 -0300

    Base kamikaze project for dead mods

---
+++ Kamikaze/ModInstance.cs
@@ -0,0 +1,116 @@
+﻿using ColossalFramework.PlatformServices;
+using ColossalFramework.UI;
+using ElectricRoads;
+using Kwytto.Interfaces;
+using Kwytto.LiteUI;
+using Kwytto.Utils;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+[assembly: AssemblyVersion("0.0.0.0")]
+namespace ElectricRoads
+{
+    public class ModInstance : BasicIUserMod<ModInstance, MainController>
+    {
+        public override string SimpleName { get; } = "Loading Screen Mod Airports temporary fix - ending";
+
+        public override string Description { get; } = "Remove it!";
+
+        protected override void SetLocaleCulture(CultureInfo culture) { }
+
+        private static readonly Dictionary<ulong, string> ModsToReplace = new Dictionary<ulong, string>
+        {
+            [2858591409L] = "Loading Screen Mod Revisited"
+        };
+
+        public new void OnEnabled()
+        {
+            if (UIView.GetAView() != null)
+            {
+                ShowModalUnsub();
+            }
+        }
+        private bool modalShown;
+        private void ShowModalUnsub()
+        {
+            if (modalShown) return;
+            modalShown = true;
+            KwyttoDialog.ShowModal(new KwyttoDialog.BindProperties
+            {
+                title = SimpleName,
+                message = "This mod is being replaced by the following mods." +
+                "You can choose subscribe them or simple unsubscribe this - in both cases, the game will be closed to avoid conflict issues.",
+                scrollText = string.Join("\n", ModsToReplace.Select(x => $" - ID {x.Key}: {x.Value}").ToArray()),
+                buttons = new KwyttoDialog.ButtonDefinition[]
+                {
+                    KwyttoDialog.SpaceBtn,
+                    new KwyttoDialog.ButtonDefinition
+                    {
+                        title = "Subscribe them all!",
+                        onClick = () =>
+                        {
+                            foreach(var id in ModsToReplace.Keys)
+                            {
+                                try {
+                                    PlatformService.workshop.Subscribe(new PublishedFileId(id));
+                                } catch {
+                                }
+                            }
+                            PlatformService.workshop.Unsubscribe(new PublishedFileId(ModId));
+                            Application.Quit();
+                            return true;
+                        }
+                    },
+                    new KwyttoDialog.ButtonDefinition
+                    {
+                        title = "Just unsubscribe this",
+                        onClick = () =>
+                        {
+                            PlatformService.workshop.Unsubscribe(new PublishedFileId(ModId));
+                            Application.Quit();
+                            return true;
+                        }
+                    }
+                }
+            });
+        }
+
+        protected override void CreateGroup9(UIHelper helper)
+        {
+            base.CreateGroup9(helper);
+            ShowModalUnsub();
+        }
+    }
+
+    public class MainController : BaseController<ModInstance, MainController>
+    {
+
+    }
+
+
+}
+namespace Kwytto
+{
+    public static class CommonProperties
+    {
+        public static bool DebugMode => ModInstance.DebugMode;
+        public static string Version => ModInstance.Version;
+        public static string FullVersion => ModInstance.FullVersion;
+        public static string ModName => ModInstance.Instance.SimpleName;
+        public static string Acronym { get; } = "LSM";
+        public static string ModRootFolder => "";
+        public static string ModIcon => ModInstance.Instance.IconName;
+        public static string ModDllRootFolder => ModInstance.RootFolder;
+
+        public static string GitHubRepoPath => null;
+
+        internal static readonly string[] AssetExtraDirectoryNames = new string[0];
+        internal static readonly string[] AssetExtraFileNames = new string[] { };
+
+        public static Color ModColor { get; } = ColorExtensions.FromRGB("017158");
+        public static float UIScale { get; } = 1f;
+    }
+}
